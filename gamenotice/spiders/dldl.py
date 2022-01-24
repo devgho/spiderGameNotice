fde5308ab2899b3d96d0e4c326a42aac129fb038
devgho <devgho@outlook.com>
2022-01-24 18:44:26 +0800

    add a new game dldl

---
+++ gamenotice/spiders/dldl.py
@@ -0,0 +1,21 @@
+from array import array
+import json,re
+import scrapy
+from gamenotice.items import GamenoticeItem
+
+
+class DldlSpider(scrapy.Spider):
+    name = 'dldl'
+    allowed_domains = ['hsdj.37.com.cn']
+    start_urls = ['https://mg-api.37.com.cn/website/articlelist/51/95?webId=51&categoryId=95&pageSize=8&pageNum=1']
+
+    def parse(self, response):
+        rs = json.loads(response.text)
+        for i in rs['data']['array'][:2]:
+            if ("更新" or "维护") in i['title']:
+                item = GamenoticeItem()
+                item['title'] = i['title']
+                item['url'] = "https://hsdj.37.com.cn/article/"+str(i['articleId'])
+                item['dtime'] = i['time']
+                item['detail'] = re.sub(r'<.*?>',"",i['content'])
+                yield item
